879456c447a0b22a59100246675209035b980804
phytomosaic <smithr2@oregonstate.edu>
2021-02-26 08:00:57 -0800

    Man updates

--- R/veg.R
+++ R/veg.R
@@ -16,7 +16,7 @@
 #' ### Study area description
 #'
 #' The Mafragh plain is in El Tarf Province of far northeast Algeria
-#' (36.84704$^{\circ}$, 7.945$^{\circ}$).  According to documentation in R
+#' (36.84704 N, 7.9450 E).  According to documentation in R
 #' package `ade4`:
 #'
 #' "This marshy coastal plain is a geomorphological feature, east of Port of
@@ -40,7 +40,7 @@
 #' accounts).  As of 2021, he continues to actively publish on the vegetation of
 #' northern Algeria.
 #'
-#' We should recognize colonialist legacies that brought us here.  France
+#' We should recognize the colonialist legacies that brought us here.  France
 #' controlled Algeria from 1830 until its independence in 1962.  Prior to that,
 #' Algeria was occupied by Ottoman Turks, Spaniards, Arab Muslims,
 #' Berber/Amazigh, Romans, Phoenicians, and prehistoric occupants dating to at
@@ -59,20 +59,20 @@
 #' @references
 #'
 #' de Bélair, G. 1981. Biogéographie et aménagement de la Plaine de la Mafragh
-#' (Annaba - Algérie) [Biogeography and development of the Mafragh Plain
-#' (Annaba, Algeria)]. Dissertation, Université Paul Valéry, Montpellier.
+#' (Annaba - Algérie) *Biogeography and development of the Mafragh Plain
+#' (Annaba, Algeria)*. Dissertation, Université Paul Valéry, Montpellier.
 #' http://www.sudoc.fr/00871262X
 #'
 #' de Bélair, G. 1990. Structure, fonctionnement et perspectives de gestion de
 #' quatre écocomplexes lacustres et marécageux (El Kala, Est-Algérien)
-#' [Structure, function and management perspectives of four lake and marsh
-#' eco-complexes (El Kala, East Algeria)]. Dissertation, Université des sciences
+#' *Structure, function and management perspectives of four lake and marsh
+#' eco-complexes (El Kala, East Algeria)*. Dissertation, Université des sciences
 #' et techniques de Montpellier 2. http://www.sudoc.fr/007729197
 #'
 #' de Bélair, G. and M. Bencheikh-Lehocine. 1987. Composition et déterminisme de
 #' la végétation d'une plaine côtière marécageuse: La Mafragh (Annaba, Algérie)
-#' [Composition and determinism of the vegetation of a marshy coastal plain: La
-#' Mafragh (Annaba, Algeria)]. Bulletin d'Ecologie, 18(4): 393–407.
+#' *Composition and determinism of the vegetation of a marshy coastal plain: La
+#' Mafragh (Annaba, Algeria)*. Bulletin d'Ecologie, 18(4): 393–407.
 #'
 #' Dray, S. and A. Dufour. 2007. The `ade4` package: implementing the duality
 #' diagram for ecologists. Journal of Statistical Software 22(4): 1-20.
